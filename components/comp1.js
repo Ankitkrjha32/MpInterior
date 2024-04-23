fadd6a56f90b92d6207777ef5c33b8b253784377
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-04-23 20:56:21 +0530

    Add files via upload

---
+++ components/comp1.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const comp1 = () => {
+  return (
+    <div className='h-full w-full bg-pink-300 mt-2 flex flex-col items-center justify-center py-5'>
+      
+      <div className='w-8/12 h-[120px] bg-orange-300 flex flex-col items-center justify-center font-edu-sa px-5  py-4'>
+                <p className='font-bold text-[20px] text-black mb-2' > 
+                        OUR SATISFACTION COMMITMENT
+                </p>
+
+                <p className='font-bold text-[15px] text-richblue-600 '>
+                     Your satisfaction is our priority. We understand that creating your ideal living space can feel  overwhelming. If you're not completely
+                </p>
+                <p className='font-bold text-[15px] text-black'>
+                      satisfied with your design for any reason at all, please reach out to us. We are committed to
+                </p>
+                <p className='font-bold text-[15px] text-richblue-600'>
+                      making things right and ensuring you love your home.
+                </p>
+
+
+          </div>
+    </div>
+  )
+}
+
+export default comp1
---
+++ components/core/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full  '>
+      <div className='bg-[#e5e7eb] flex justify-around pt-8 pb-5 ' >
+            <div className='flex gap-[80px]'>
+                <div className='flex flex-col'>
+                    <p className='font-bold text-[22px] font-mono '>Mp Design & Decorations</p>
+                    
+                </div>
+                <div className='flex flex-col'>
+                    <p className='font-bold text-[20px] font-mono '>Company</p>
+                    <p>Careers</p>
+                    <p>Blog</p>
+                    <p>Contacts</p>
+                </div>
+            </div>
+            <div className='flex gap-[80px] '> 
+                <div className='flex flex-col items-center'>
+                    <p className='font-bold text-[21px] font-mono '>Working Hours</p>
+                    <p>Monday - Friday </p>
+                       <p> 8:00Am-5:00PM</p>
+                    <p>Sunday :Closed</p>
+                </div>
+                <div className=' flex flex-col'>
+                    <p className='font-bold text-[22px] font-mono '>Subscription</p>
+                    <p>Subscribe your Email address for Latest Updates</p>
+                    <input type="email" name="eemail" id="email" placeholder='Email'
+                    className='h-8 bg-white border-2 mt-2 placeholder:px-2 text-[18px]'/>
+                    <button className='bg-yellow-700  text-white font-bold mt-1 px-10 py-1'>
+                        Subscribe
+                    </button>
+                </div>
+            </div>
+      </div>
+      {/* <div>hello</div> */}
+    </div>
+  )
+}
+
+export default Footer
